c519049daa1c55ff537057e8768de76bf38461df
Pedro Henrique <phbb1@aluno.ifal.edu.br>
2023-01-30 22:52:03 -0300

    Add Food model (#18)

---
+++ src/core/domain/models/food/unit.spec.ts
@@ -0,0 +1,46 @@
+import FoodCategory from "@typing/FoodCategory";
+import Food from "./Food";
+
+describe("Food model", () => {
+    it("should create an instance of Food", () => {
+        const food = new Food();
+
+        expect(food.id).not.toBeUndefined(); 
+        expect(food.name).not.toBeUndefined(); 
+        expect(food.description).not.toBeUndefined(); 
+        expect(food.image).not.toBeUndefined(); 
+        expect(food.price).not.toBeUndefined(); 
+        expect(food.category).not.toBeUndefined(); 
+        expect(food.category).toEqual(FoodCategory.BEBIDA); 
+        expect(food.ingredients).not.toBeUndefined(); 
+    });
+
+    it("should create an instance of Food from a JSON mock", () => {
+        const foodMock = {
+            id: "1",
+            name: "Refri",
+            description: "A refreshing drink",
+            image: "image.png",
+            price: 4,
+            category: 0,
+            ingredients: "Water, sugar and carbon dioxide",
+        };
+
+        const food = Food.fromJSON(foodMock);
+        expect(food).not.toBeUndefined();
+        expect(food.id).not.toBeUndefined();
+        expect(food.id).toBe(foodMock.id);
+        expect(food.name).not.toBeUndefined();
+        expect(food.name).toBe(foodMock.name);
+        expect(food.description).not.toBeUndefined();
+        expect(food.description).toBe(foodMock.description);
+        expect(food.image).not.toBeUndefined();
+        expect(food.image).toBe(foodMock.image);
+        expect(food.price).not.toBeUndefined();
+        expect(food.price).toBe(foodMock.price);
+        expect(food.category).not.toBeUndefined();
+        expect(food.category).toBe(foodMock.category);
+        expect(food.ingredients).not.toBeUndefined();
+        expect(food.ingredients).toBe(foodMock.ingredients);
+    });
+})
---
+++ src/core/domain/types/FoodCategory/index.ts
@@ -0,0 +1,7 @@
+enum FoodCategory {
+    BEBIDA = 0,
+    SOBREMESA = 1,
+    REFEICAO = 2,
+}
+
+export default FoodCategory;
