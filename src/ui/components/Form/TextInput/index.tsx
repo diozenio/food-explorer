70ca880ee374eee8bf1e1f9fa712111364af10ea
Pedro Henrique <phbb1@aluno.ifal.edu.br>
2023-02-24 14:31:11 -0300

    Fix autocomplete in TextInput (#27)

--- src/ui/components/Form/TextInput/index.tsx
+++ src/ui/components/Form/TextInput/index.tsx
@@ -24,6 +24,7 @@ export function TextInput({ id, label, labelStyle, prefixIcon, style, ...props }
 
                 <input
                     id={id}
+                    autoComplete="off"
                     {...props}
                 />
             </div>
